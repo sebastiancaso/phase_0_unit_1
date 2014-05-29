03b4f7f3fcdf0d88a3367201a76702a1d0e62041
sebastian caso <sebcaso@gmail.com>
2014-05-28 20:48:32 -0400

    group project pseudocode

---
+++ week_3/6_group_project/gc1_initial_solution.js
@@ -0,0 +1,23 @@
+// My role in the group is: Person 2 (Robert Schwartz and Sebastian Caso)
+
+// Here is my part of the challenge:
+/*our assumption is we are given the numbers and don't need to create a new array*/
+
+// 
+// #pseudocode
+// create variable called total which is equal to 0
+// loop through the number array (which was given to us) 
+// pass the number array to a block which will add all the numbers
+
+// 
+// #pseudocode
+// Create new variable named mean 
+// Have it equal to total / length of the array 
+
+// 
+// #pseudocode 
+// First put the array in order 
+// then find the length of the array
+// then divide by 2 
+// if even, subtract 1 and use that number to call that location in the array.
+// if there's a remainder, round down and use that number to call the location in the array.  
